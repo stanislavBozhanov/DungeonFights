674fa5cfd9598f080046eb03647db42790342137
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2015-01-04 13:59:16 +0200

    New methods for Hero class

--- src/Hero.java
+++ src/Hero.java
@@ -2,13 +2,15 @@
  * Created by stenly on 04.01.15.
  */
 public class Hero {
-    public Integer health;
+    public double health;
     public String name;
     public String nickname;
+    public final double MAX_HEALTH;
 
-    public Hero(String name, Integer health, String nickname) {
+    public Hero(String name, double health, String nickname) {
         this.name = name;
         this.health = health;
+        this.MAX_HEALTH = health;
         this.nickname = nickname;
     }
 
@@ -16,5 +18,29 @@ public class Hero {
         return String.format("%s the %s", this.name, this.nickname);
     }
 
+    public double getHealth() {
+        return this.health;
+    }
 
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    public void takeDamage(double damagePoints) {
+        this.health -= damagePoints;
+        if (this.health < 0.0) {
+            this.health = 0.0;
+        }
+    }
+
+    public boolean takeHealing(double healingPoints) {
+        if (!isAlive()) {
+            return false;
+        }
+        this.health += healingPoints;
+        if (this.health > MAX_HEALTH) {
+            this.health = MAX_HEALTH;
+        }
+        return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,29 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Hero stenly = new Hero("Stenly", 100, "StenSten");
+        Hero stenly = new Hero("Stenly", 100.0, "StenSten");
+
         System.out.println(stenly.name);
         System.out.println(stenly.health);
         System.out.println(stenly.nickname);
 
         System.out.println(stenly.knownAs());
+        System.out.println(stenly.getHealth());
+        System.out.println(stenly.isAlive());
+
+        stenly.takeDamage(99.9);
+
+        System.out.println(stenly.getHealth());
+
+        stenly.takeDamage(0.2);
+
+        System.out.println(stenly.getHealth());
+        System.out.println(stenly.isAlive());
+        System.out.println(stenly.takeHealing(10.0));
+
+        stenly.health = 10.0;
+        stenly.takeHealing(20.0);
+
+        System.out.println(stenly.getHealth());
     }
 }
